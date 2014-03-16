6d4a0efb27ccb936756d0f185cfab94cfe008355
Caitlin Steinert <caitlinsteinert@gmail.com>
2014-03-16 00:13:49 -0400

    Created code for the gusset and foot work, refactored some existing code for more general use. Stopped using hard-coded values.
    Wow.

--- js/sock-calculator.js
+++ js/sock-calculator.js
@@ -12,9 +12,9 @@ $(document).ready(function () {
             var calcLegRows = gaugeRows*legLength;
             return calcLegRows;
         },
-        heelStitches: function (cuffStitches) {
-            var calcHeelStitches = cuffStitches/2;
-            return calcHeelStitches;
+        halfCuffStitches: function (cuffStitches) {
+            var calcHalfCuffStitches = cuffStitches/2;
+            return calcHalfCuffStitches;
         },
         heelRows: function (gaugeRows, heelLength) {
             var calcHeelRows = gaugeRows*heelLength;
@@ -23,13 +23,13 @@ $(document).ready(function () {
             }
             return calcHeelRows;
         },
-        heelTurn: function (heelStitches) {
+        heelTurn: function (halfCuffStitches) {
             var heelDecreaseStitches;
             var calcHeelTurnBase = 10;
-            if ((heelStitches - calcHeelTurnBase) % 2 > 0) {
+            if ((halfCuffStitches - calcHeelTurnBase) % 2 > 0) {
                 calcHeelTurnBase -= 1;
             }
-            heelDecreaseStitches = (heelStitches - calcHeelTurnBase) / 2;
+            heelDecreaseStitches = (halfCuffStitches - calcHeelTurnBase) / 2;
             return {
                 heelTurnStitches: calcHeelTurnBase,
                 heelDecreaseStitches: heelDecreaseStitches
@@ -38,30 +38,54 @@ $(document).ready(function () {
         gussetPickUpStitches: function (heelLength, gaugeRows) {
             var calcGussetPickUpStitches = gaugeRows*heelLength;
             return calcGussetPickUpStitches;
+        },
+        gussetDecrease: function (halfCuffStitches, gussetPickUpStitches, heelTurnStitches) {
+            var calcGussetRemainderStitches = halfCuffStitches + (gussetPickUpStitches * 2) + heelTurnStitches;
+            var calcGussetDecreaseRows = calcGussetRemainderStitches - (halfCuffStitches * 2);
+            return {
+                gussetRemainderStitches: calcGussetRemainderStitches,
+                gussetDecreaseRows: calcGussetDecreaseRows
+            };
+        },
+        footRows: function (gaugeRows, footLength, gussetDecreaseRows) {
+            var calcFootRows = (footLength*gaugeRows) - gussetDecreaseRows;
+            return calcFootRows;
         }
     };
     
     $('#calculate').click(function() {
         var stitchesPerInch = parseFloat($('input[name=stitchesPerInch]').val());
+        var rowsPerInch = parseFloat($('input[name=rowsPerInch]').val());
         var circumferenceOfCuff = parseFloat($('input[name=circumferenceOfCuff]').val());
+        var lengthOfCuff = parseFloat($('input[name=lengthOfCuff]').val());
+        var lengthOfLeg = parseFloat($('input[name=lengthOfLeg]').val());
+        var lengthOfHeel = parseFloat($('input[name=lengthOfHeel]').val());
+        var lengthOfFoot = parseFloat($('input[name=lengthOfFoot]').val());
+        var lengthOfToe = parseFloat($('input[name=lengthOfToe]').val());
+
 
         var numberOfCuffStitches = stitchCalculator.cuffStitches (stitchesPerInch, circumferenceOfCuff);
-        var numberOfCuffRows = stitchCalculator.cuffRows (3, 3);
-        var numberOfLegRows = stitchCalculator.legRows (3, 5);
-        var numberOfHeelStitches = stitchCalculator.heelStitches(numberOfCuffStitches);
-        var numberOfHeelRows = stitchCalculator.heelRows (3, 5);
-        var heelTurn = stitchCalculator.heelTurn(numberOfHeelStitches);
-        var numberOfGussetPickUpStitches = stitchCalculator.gussetPickUpStitches(3, 5);
+        var numberOfCuffRows = stitchCalculator.cuffRows (rowsPerInch, lengthOfCuff);
+        var numberOfLegRows = stitchCalculator.legRows (rowsPerInch, lengthOfLeg);
+        var halfOfCuffStitches = stitchCalculator.halfCuffStitches(numberOfCuffStitches);
+        var numberOfHeelRows = stitchCalculator.heelRows (rowsPerInch, lengthOfHeel);
+        var heelTurn = stitchCalculator.heelTurn(halfOfCuffStitches);
+        var numberOfGussetPickUpStitches = stitchCalculator.gussetPickUpStitches(lengthOfHeel, rowsPerInch);
+        var gussetDecrease = stitchCalculator.gussetDecrease(halfOfCuffStitches,numberOfGussetPickUpStitches,heelTurn.heelTurnStitches);
+        var footRows = stitchCalculator.footRows(rowsPerInch,lengthOfFoot,gussetDecrease.gussetDecreaseRows);
         
         $(".cuff-stitches").text(numberOfCuffStitches);
         $(".cuff-rows").text(numberOfCuffRows);
         $(".leg-rows").text(numberOfLegRows);
-        $(".heel-stitches").text(numberOfHeelStitches);
+        $(".heel-stitches").text(halfOfCuffStitches);
         $(".heel-rows").text(numberOfHeelRows);
         $(".heel-base").text(heelTurn.heelTurnStitches);
         $(".split-heel-stitches").text(heelTurn.heelTurnStitches + heelTurn.heelDecreaseStitches - 1);
         $(".heel-work").text(heelTurn.heelTurnStitches - 1);
         $(".gusset-pick-up-stitches").text(numberOfGussetPickUpStitches);
+        $(".gusset-remainder-stitches").text(gussetDecrease.gussetRemainderStitches);
+        $(".gusset-decrease-rows").text(gussetDecrease.gussetDecreaseRows);
+        $(".foot-work").text(footRows);
     });
 
     $("form").submit(function () {
